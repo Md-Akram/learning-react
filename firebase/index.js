8b5843f65680f94a89e929b2592950c962868117
md-akram <akramkaikobad@gmail.com>
2023-04-11 01:02:40 +0600

    trying out some concepts

---
+++ firebase/index.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBM80pmQtolGtiM6OkE4vlexhckaTpfdWs",
+    authDomain: "react-learning-project-9c7ea.firebaseapp.com",
+    projectId: "react-learning-project-9c7ea",
+    storageBucket: "react-learning-project-9c7ea.appspot.com",
+    messagingSenderId: "785222390857",
+    appId: "1:785222390857:web:9e87b65231924dcd335b98",
+    databaseURL: 'https://react-learning-project-9c7ea-default-rtdb.asia-southeast1.firebasedatabase.app/'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
--- learning-react/src/App.jsx
+++ learning-react/src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import { getDatabase, ref, set, onValue, push } from "firebase/database";
+import { app } from '../../firebase';
+
 import './App.css'
 
-const products = [
-  {
-    name: "Product 1",
-    price: 10.99,
-    description: "This is product 1"
-  },
-  {
-    name: "Product 2",
-    price: 24.99,
-    description: "This is product 2"
-  },
-  {
-    name: "Product 3",
-    price: 7.50,
-    description: "This is product 3"
-  }
-];
+// const products = [
+//   {
+//     name: "Product 1",
+//     price: 10.99,
+//     description: "This is product 1"
+//   },
+//   {
+//     name: "Product 2",
+//     price: 24.99,
+//     description: "This is product 2"
+//   },
+//   {
+//     name: "Product 3",
+//     price: 7.50,
+//     description: "This is product 3"
+//   }
+// ];
 
 // const Counter = ({ initialValue }) => {
 //   const [value, setValue] = useState(initialValue)
@@ -41,43 +45,107 @@ const products = [
 //   )
 // }
 
-const Product = ({ product, handleNumberOfProducts }) => {
+// const Product = ({ product, handleNumberOfProducts }) => {
 
-  const [isAddedToCart, setIsAddedToCart] = useState(false)
+//   const [isAddedToCart, setIsAddedToCart] = useState(false)
 
-  const handleAddToCart = () => {
-    setIsAddedToCart(true)
-    handleNumberOfProducts()
-  }
-  return (
-    <>
-      <h1>{product.name}</h1>
-      <h2>{product.price}</h2>
-      <h3>{product.description}</h3>
-      <button onClick={handleAddToCart} disabled={isAddedToCart}>
-        {isAddedToCart ? 'Added to Cart' : "Add to Cart"}
-      </button>
-    </>
-  )
-}
+//   const handleAddToCart = () => {
+//     setIsAddedToCart(true)
+//     handleNumberOfProducts()
+//   }
+//   return (
+//     <>
+//       <h1>{product.name}</h1>
+//       <h2>{product.price}</h2>
+//       <h3>{product.description}</h3>
+//       <button onClick={handleAddToCart} disabled={isAddedToCart}>
+//         {isAddedToCart ? 'Added to Cart' : "Add to Cart"}
+//       </button>
+//     </>
+//   )
+// }
 
-const ProductList = ({ products }) => {
-  const [numberOfProducts, setNumberOfProducts] = useState(0)
-  const handleNumberOfProducts = () => {
-    setNumberOfProducts(numberOfProducts + 1)
-  }
-  return (
-    <>
-      {products.map((product) => <Product product={product} handleNumberOfProducts={handleNumberOfProducts} />)}
-      {numberOfProducts > 0 && <h1>{numberOfProducts} products added to the cart</h1>}
-    </>
-  )
-}
+// const ProductList = ({ products }) => {
+//   const [numberOfProducts, setNumberOfProducts] = useState(0)
+//   const handleNumberOfProducts = () => {
+//     setNumberOfProducts(numberOfProducts + 1)
+//   }
+//   return (
+//     <>
+//       {products.map((product) => <Product product={product} handleNumberOfProducts={handleNumberOfProducts} />)}
+//       {numberOfProducts > 0 && <h1>{numberOfProducts} products added to the cart</h1>}
+//     </>
+//   )
+// }
 
 function App() {
+
+  const [info, setInfo] = useState()
+
+  const [userData, setUserData] = useState({ newName: "", newMessage: "" })
+
+  useEffect(readData, [])
+
+  const writeMessageData = (arg) => {
+    const db = getDatabase(app);
+    const msgListRef = ref(db, '/messages')
+    const newMsgRef = push(msgListRef)
+    set(newMsgRef, arg);
+  }
+
+
+
+  function readData() {
+    const db = getDatabase(app);
+    const usersRef = ref(db, '/messages');
+    onValue(usersRef, (snapshot) => {
+      const data = snapshot.val();
+      const dataArray = Object.keys(data).map(key => data[key]);
+      setInfo(dataArray)
+
+    });
+  }
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (userData.newMessage === '' && userData.newName === '') return
+    writeMessageData(userData)
+    setUserData({ newName: "", newMessage: "" })
+  }
+
+
+
+
   return (
     // <Counter initialValue={Number(prompt('provide initial value'))} />
-    <ProductList products={products} />
+    // <ProductList products={products} />
+    // <div>{data && data.map(user => <h1>{user.name}</h1>)}</div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">write your name</label>
+        <input
+          type="text"
+          name="newName"
+          id="name"
+          value={userData.newName}
+          onChange={handleChange} />
+        <br />
+        <label htmlFor="message">Your message</label>
+        <input
+          type="text"
+          name="newMessage"
+          id="message"
+          value={userData.newMessage}
+          onChange={handleChange} />
+        <button type="submit">send</button>
+      </form>
+
+      {info && info.map((obj) => <p>{obj.newName}:{obj.newMessage}</p>)}
+    </>
   )
 }
 
